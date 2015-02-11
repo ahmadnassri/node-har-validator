38d956a746b0273f4ac0abfdd002e96b7db9b725
Ahmad Nassri <ahmad@ahmadnassri.com>
2015-02-11 13:32:43 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ var validator = require('is-my-json-valid/require');
 var validate = validator('./schema.json');
 
 module.exports = function (data, cb) {
+  if (!data) {
+    return false;
+  }
+  
+  // execute is-my-json-valid
   var valid = validate(data);
 
+  // callback?
   if (cb) {
     return cb(validate.errors, valid);
   }
