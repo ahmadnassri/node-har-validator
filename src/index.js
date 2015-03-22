92fe4cebbdbbaacd733925694bb959a348d0a0a7
Ahmad Nassri <ahmad@codeinchaos.com>
2015-03-21 22:56:13 -0700

    mistake, should be sending valid back

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ var runner = function (schema, data, cb) {
   // callback?
   if (!cb) {
     if (validate.errors) {
-      return false
+      return valid
     }
   } else {
     return cb(validate.errors ? new ValidationError(validate.errors) : null, valid)
